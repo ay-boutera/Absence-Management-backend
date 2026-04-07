fcaeee0acb1e76952613bd8315bdeb2aad1f0a80
Rim <bouhafsrim2005@gmail.com>
2026-04-07 13:27:15 +0100

    rah nsupp

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ from fastapi.responses import JSONResponse
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.middlewares.security import security_headers
 from app.config import settings
